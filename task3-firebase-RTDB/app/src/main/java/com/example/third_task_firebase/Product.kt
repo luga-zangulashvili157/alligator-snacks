29ee8f8610142290b3e71ad13489cee56a3af4d5
luga-zangulashvili157 <lugazangulashvili@gmail.com>
2025-12-14 13:19:10 -0800

    Delete task3-firebase-RTDB directory

--- task3-firebase-RTDB/app/src/main/java/com/example/third_task_firebase/Product.kt
+++
@@ -1,9 +0,0 @@
-package com.example.third_task_firebase
-
-
-data class Product(
-    var id: String? = null,
-    var name: String? = null,
-    var price: Double? = null,
-    var inStock: Boolean? = null
-)
